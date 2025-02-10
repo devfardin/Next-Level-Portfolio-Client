d399b96392ca6cc5c4b9887240f44fb240802ada
devfardin <contactfardin22@gmail.com>
2025-02-11 01:04:23 +0600

    make button components

--- Client/src/components/shared/Navbar.tsx
+++ Client/src/components/shared/Navbar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
   const pathname = usePathname();
   console.log(pathname);
-  
+
   const menuItems = [
     {
       label: 'Home',
@@ -42,32 +42,26 @@ const Navbar = () => {
           href="/"
           className="ml-4 text-xl font-semibold text-gray-800 hover:text-gray-600"
         >
-          <Image src={logo} width={200} height={150} alt="Fardin's Logo"/>
+          <Image src={logo} width={200} height={150} alt="Fardin's Logo" />
         </Link>
       </div>
 
       <div className="hidden lg:flex">
         <ul className="flex space-x-6 text-gray-800">
           {
-            menuItems?.map((item, index)=> <li key={index} className="hover:text-gray-600">
-            <Link 
-            className={`${
-              pathname === item.link
+            menuItems?.map((item, index) => <li key={index} className="hover:text-gray-600">
+              <Link className={`${pathname === item.link
                 ? "text-primery font-medium text-lg"
                 : "text-gray-700 hover:text-primery text-lg"
-            }`}
-            href={`${item.link}`}> {item.label} </Link>
-          </li> )
+                }`}
+                href={`${item.link}`}> {item.label} </Link>
+            </li>)
           }
         </ul>
       </div>
 
       <div className="flex items-center">
-        <Link
-          href="/login"
-          className="border border-teal-500 text-teal-500 px-5 py-2 rounded-full hover:bg-teal-500 hover:text-black transition duration-200">
-          Login
-        </Link>
+        
       </div>
     </div>
   );
