def6cd49e87d303a48031fcecf73e00e9fc31d35
devfardin <contactfardin22@gmail.com>
2025-02-12 16:32:18 +0600

    fixed header and footer

--- Client/src/components/shared/Footer.tsx
+++ Client/src/components/shared/Footer.tsx
@@ -28,15 +28,15 @@ const Footer = () => {
     },
   ]
   return (
-    <div className='bg-black py-10 lg:py-20'>
+    <div className='bg-black pt-10 lg:pt-20'>
       <Container>
-      <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-10 lg:gap-32 justify-between'>
+        <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 gap-10 lg:gap-20 justify-between mb-10'>
           {/* First items */}
           <div className='flex flex-col gap-5 sm:col-span-6 xl:col-span-2'>
             <Image alt="Fardin's Logo" src={logo} width={300} height={200} />
             <p className='font-normal text-base text-white mt-3'>Fardin Ahmed is a skilled web developer specializing in Next.js, React, WordPress, and custom web solutions. With expertise in e-commerce, plugin development, and website optimization, he builds high-performance, user-friendly digital experiences. 🚀</p>
-            <a className='font-medium text-white text-xl md:text-2xl hover:text-primery duration-300 transition-colors' 
-            href="mailto:contactfardin22@gmail.com">
+            <a className='font-medium text-white text-xl md:text-2xl hover:text-primery duration-300 transition-colors'
+              href="mailto:contactfardin22@gmail.com">
               contactfardin22@gmail.com
             </a>
           </div>
@@ -86,8 +86,10 @@ const Footer = () => {
           </div>
         </div>
       </Container>
-      <div className='container mx-auto '>
-        
+      <div className='py-6 text-center border-t border-[#515151]'>
+        <p className='text-base font-normal text-white'>
+          © { new Date().getFullYear() } Fardin Ahmed. All Rights Reserved.
+        </p>
       </div>
     </div>
   )
